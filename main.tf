a1d9dfb68f066a9f0141917c753d6c742bb0de3e
Roger Berlind <roger@hashicorp.com>
2017-12-09 01:45:29 -0500

    git::ssh://git@github.com/hashicorp-modules/ssh-keypair-data.git

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "ssh_key_data" {
-  source = "git@github.com:hashicorp-modules/ssh-keypair-data.git"
+  source = "git::ssh://git@github.com/hashicorp-modules/ssh-keypair-data.git"
 }
 
 output "private_key_pem" {
